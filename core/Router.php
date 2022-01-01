a895f0acf5365882b1867ed7f995de2e2cc3dbbf
Omar Ahmed <omarking05@gmail.com>
2022-01-01 03:10:07 +0200

    chore(router): document Router class

--- core/Router.php
+++ core/Router.php
@@ -2,6 +2,11 @@
 
 namespace app\core;
 
+/**
+ * Router is responsible for handling or creating any of the application routes
+ *
+ * @class Router
+ */
 class Router
 {
   protected array $routes = [];
@@ -26,43 +31,64 @@ class Router
     $this->routes[$method][$path] = $callback;
   }
 
+  /**
+   * This function responsible for resolving current path and compare it against application routes
+   * 
+   * In case route doesn't exist, it will render the 404 page
+   *
+   * Routes can be 3 types:
+   *    - If the callback is a 'string', then resolve function will expect a view file
+   *    - If the callback is 'array', then resolve function will expect a controller and it will call this method
+   *    - If the callback is a 'function', then resolve function will just call this function
+   */
   public function resolve()
   {
     $path = $this->request->getPath();
     $method = $this->request->getMethod();
     $callback = $this->routes[$method][$path] ?? false;
 
+    // In case no callback for this path, just render 404 page
     if (!$callback) {
       $this->response->setStatusCode(404);
       return $this->renderView('_404');
     }
-    
+
+    // Handle rendering only views
     if (is_string($callback)) {
       return $this->renderView($callback);
     }
 
+    // Handle rendering controller actions
     if (is_array($callback)) {
       Application::$app->controller = new $callback[0]();
       $callback[0] = Application::$app->controller;
     }
 
+    // Here handle any callback closure function
     return call_user_func($callback, $this->request, $this->response);
   }
 
-  public function renderView(string $view, array $params = null)
+  /**
+   * This will render certain view file, given an optional parameters to inject into the runtime of the view file
+   * It is used mostly by routes actions/callbacks
+   *
+   * @param string $view
+   * @param array|null $params
+   * @return string
+   */
+  public function renderView(string $view, array $params = null): string
   {
     $layoutContent = $this->layoutContent();
     $viewContent = $this->renderOnlyView($view, $params);
     return str_replace('{{content}}', $viewContent, $layoutContent);
   }
 
-  public function renderContent(string $viewContent)
-  {
-    $layoutContent = $this->layoutContent();
-    return str_replace('{{content}}', $viewContent, $layoutContent);
-  }
-
-  protected function layoutContent()
+  /**
+   * Get the layout view content, layout retrieved from controller
+   *
+   * @return string
+   */
+  protected function layoutContent(): string
   {
     $layout = Application::$app->controller->layout;
     ob_start();
@@ -70,13 +96,20 @@ class Router
     return ob_get_clean();
   }
 
+  /**
+   * This will render only the view content aside from the layout and will handle passing parameters to view file if any
+   *
+   * @param string $view
+   * @param array|null $params
+   * @return string
+   */
   protected function renderOnlyView(string $view, array $params = null)
   {
     foreach($params as $key => $value) {
       $$key = $value;
     }
 
-    // Here start capturing the output buffer instead of outputing it directly into the page
+    // Here start capturing the output buffer instead of outputting it directly into the page
     ob_start();
     include_once Application::$ROOT_DIR."/views/$view.php";
     // Then just here we get what was outputted to the buffer cleaned and we return it
