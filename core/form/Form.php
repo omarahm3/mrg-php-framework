f79bbaec0aa58d76a99c0e9a66f1b0a78a0588e2
Omar Ahmed <omarking05@gmail.com>
2022-01-01 06:54:20 +0200

    chore(form): add code documentation

--- core/form/Form.php
+++ core/form/Form.php
@@ -4,6 +4,11 @@ namespace app\core\form;
 
 use app\core\Model;
 
+/**
+ * Used to create and render a form and its fields
+ *
+ * @class Form
+ */
 class Form
 {
   public static function begin(string $action, string $method): Form
@@ -17,8 +22,15 @@ class Form
     echo '</form>';
   }
 
-  public function field(Model $model, string $attribute): Field
+  /**
+   * Will create new form field and return it
+   *
+   * @param Model $model
+   * @param string $attribute
+   * @return Field
+   */
+  public function field(Model $model, string $propertyName): Field
   {
-    return new Field($model, $attribute);
+    return new Field($model, $propertyName);
   }
 }
